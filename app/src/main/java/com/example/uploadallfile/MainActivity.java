22b5f2f81a812c5f7a67bff29679dc3050f37e3f
devmarufurrahaman <mdmarufur5682@gmail.com>
2023-11-08 17:22:37 +0600

    not complete

--- app/src/main/java/com/example/uploadallfile/MainActivity.java
+++ app/src/main/java/com/example/uploadallfile/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.uploadallfile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int REQ_CODE  = 100;
+    Button selectBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        selectBtn = findViewById(R.id.selectFile);
+        
+        selectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFile();
+            }
+        });
+    }
+
+    private void getFile() {
+        Intent intent = new Intent();
+        intent.setType("*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"SELECT FILE"),REQ_CODE);
     }
 }
